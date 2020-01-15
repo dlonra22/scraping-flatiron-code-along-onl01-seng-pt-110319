98aba68abc065b71d7c0c7e9ef8e463707af1efb
arnie b <arnold.bwaila@gmail.com>
2020-01-15 21:40:41 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,11 +26,19 @@ class Scraper
   #  end
    # binding.pry 
   end
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
   
   def get_courses
     self.get_page.css(".post")
   end
-
+  
 
   
 end
